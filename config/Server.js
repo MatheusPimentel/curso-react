9ab822f6085449d9383464ef15e4c2a227071a70
Matheus <matheus200713@gmail.com>
2018-10-05 17:24:56 -0400

    ...

--- config/Server.js
+++ config/Server.js
@@ -1,5 +1,12 @@
 const express = require("express");
+const mongoose = require("mongoose");
+const keys = require("./Keys");
 require("../src/utils/PassportGoogle");
+
+mongoose.connect(
+  keys.mongoURI,
+  { useNewUrlParser: true }
+);
 
 const app = express();
 require("../src/routes/authRoutes")(app);
